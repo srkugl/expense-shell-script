3739202bfc77173ab0eef5fb3af6c841e21413e4
sivaram <sivaram0434@gmail.com>
2024-06-28 16:42:14 +0530

    adding clonnig

--- backend.sh
+++ backend.sh
@@ -5,29 +5,26 @@ source ./common.sh
 check_root
 update_packages
 
-dnf module disable nodejs -y >> $LOGFILE 2>&1
+dnf module disable nodejs -y >> "$LOGFILE" 2>&1
 VALIDATE $? "Disabling default NodeJS module"
 
-dnf module enable nodejs:20 -y >> $LOGFILE 2>&1
+dnf module enable nodejs:20 -y >> "$LOGFILE" 2>&1
 VALIDATE $? "Enabling NodeJS 20 module"
 
-dnf install nodejs -y >> $LOGFILE 2>&1
+dnf install nodejs -y >> "$LOGFILE" 2>&1
 VALIDATE $? "Installing NodeJS 20"
 
-useradd expense >> $LOGFILE 2>&1
+useradd expense >> "$LOGFILE" 2>&1
 VALIDATE $? "Adding user 'expense'"
 
-mkdir /app >> $LOGFILE 2>&1
+mkdir /app >> "$LOGFILE" 2>&1
 VALIDATE $? "Creating /app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip >> $LOGFILE 2>&1
-VALIDATE $? "Downloading backend code"
+git clone https://github.com/srkugl/expense-backend.git /app >> "$LOGFILE" 2>&1
+VALIDATE $? "Cloning backend code from GitHub to /app directory"
 
-cd /app >> $LOGFILE 2>&1
-unzip /tmp/backend.zip >> $LOGFILE 2>&1
-VALIDATE $? "Unzipping backend code"
-
-npm install >> $LOGFILE 2>&1
+cd /app >> "$LOGFILE" 2>&1
+npm install >> "$LOGFILE" 2>&1
 VALIDATE $? "Installing application dependencies"
 
 cat <<EOF > /etc/systemd/system/backend.service
@@ -45,19 +42,22 @@ WantedBy=multi-user.target
 EOF
 VALIDATE $? "Creating backend service file"
 
-dnf install mysql -y >> $LOGFILE 2>&1
-VALIDATE $? "Installing MySQL client"
-
-mysql -h expense.db.test.ullagallu.cloud -uroot -pExpenseApp@1 < /app/schema/backend.sql >> $LOGFILE 2>&1
-VALIDATE $? "Loading database schema"
-
-systemctl daemon-reload >> $LOGFILE 2>&1
+systemctl daemon-reload >> "$LOGFILE" 2>&1
 VALIDATE $? "Reloading systemd daemon"
 
-systemctl start backend >> $LOGFILE 2>&1
+systemctl start backend >> "$LOGFILE" 2>&1
 VALIDATE $? "Starting backend service"
 
-systemctl enable backend >> $LOGFILE 2>&1
+systemctl enable backend >> "$LOGFILE" 2>&1
 VALIDATE $? "Enabling backend service"
+
+dnf install mysql -y >> "$LOGFILE" 2>&1
+VALIDATE $? "Installing MySQL client"
+
+mysql -h expense.db.test.ullagallu.cloud -uroot -pExpenseApp@1 < /app/schema/backend.sql >> "$LOGFILE" 2>&1
+VALIDATE $? "Loading database schema"
+
+systemctl restart backend >> "$LOGFILE" 2>&1
+VALIDATE $? "Restarting backend service"
 
 echo -e "$G All tasks completed successfully! $N"
--- frontend.sh
+++ frontend.sh
@@ -20,20 +20,16 @@ VALIDATE $? "Checking Nginx default content"
 rm -rf /usr/share/nginx/html/* >> $LOGFILE 2>&1
 VALIDATE $? "Removing default Nginx content"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip >> $LOGFILE 2>&1
-VALIDATE $? "Downloading frontend content"
+git clone https://github.com/srkugl/expense-frontend.git /usr/share/nginx/html >> "$LOGFILE" 2>&1
+VALIDATE $? "Cloning frontend code from GitHub to Nginx directory"
 
-cd /usr/share/nginx/html >> $LOGFILE 2>&1
-unzip /tmp/frontend.zip >> $LOGFILE 2>&1
-VALIDATE $? "Unzipping frontend content"
-
-curl -I http://localhost | grep "200 OK" >> $LOGFILE 2>&1
+curl -I http://localhost | grep "200 OK" >> "$LOGFILE" 2>&1
 VALIDATE $? "Checking Nginx frontend content"
 
 cat <<EOF > /etc/nginx/default.d/expense.conf
 proxy_http_version 1.1;
 
-location /api/ { proxy_pass http://localhost:8080/; }
+location /api/ { proxy_pass http://expense.backend.test.ullagallu.cloud:8080/; }
 
 location /health {
   stub_status on;
@@ -42,7 +38,12 @@ location /health {
 EOF
 VALIDATE $? "Creating Nginx reverse proxy configuration"
 
-systemctl restart nginx >> $LOGFILE 2>&1
+systemctl restart nginx >> "$LOGFILE" 2>&1
 VALIDATE $? "Restarting Nginx"
 
 echo -e "$G All tasks completed successfully! $N"
+
+
+
+
+
